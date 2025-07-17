394b5b0f12fae8292c9618f619be985079074f62
zainabturkmen <zainabturkmen7@gmail.com>
2025-07-17 12:04:24 +0430

    typescript

---
+++ src/tutorial.ts
@@ -0,0 +1,13 @@
+console.log("typscript tutorial");
+
+interface someValue {
+  name: string;
+  id: number;
+}
+
+let someObj: someValue = {
+  name: "random",
+  id: 123,
+};
+
+console.log(someObj);
