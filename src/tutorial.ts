e7a8da35cea10cb1267e1d02b3c40abb3ec8a35a
zainabturkmen <zainabturkmen7@gmail.com>
2025-11-06 20:07:33 +0430

    type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,15 +1,21 @@
-let greeting: string = "Hello";
-
-greeting = greeting.toLocaleLowerCase();
-
-console.log(greeting);
-
-let amount:number = 20-2
-
-console.log(amount);
-
-let isamount:boolean = amount >= 20
-
-console.log(isamount);
 
 
+
+let tax:number | string = 10
+tax = 100
+tax = "$10 "
+
+
+let requestStatus: "pending" | "success" | "error" = "pending"
+
+requestStatus = "success"
+requestStatus = "error"
+// requestStatus = "random"
+
+let notSure:any = 4
+
+notSure = "maybe a string instead"
+
+notSure = false
+
+let random;
