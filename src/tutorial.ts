6511d82dc385d3ab919cb955e12998b9d3a0f596
zainabturkmen <zainabturkmen7@gmail.com>
2025-11-09 09:46:13 +0430

    type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,20 +1,4 @@
-const books = ["1284", "Brave New World", "Frhrenheight 451"];
-
-let foundbooks: string | undefined;
-
-for (let book of books) {
-  if (book === "1284") {
-    foundbooks = book;
-    foundbooks.length;
-    break;
-  }
-}
-
-let disscount: number | string = 20;
-disscount = "20"
-// disscount = true
-
-let orderStatues: "processing" | "shippping" | "delivered" = "processing"
-
-orderStatues = "delivered"
-// orderStatues = "value"
+const user = {
+  name: "Zainab ",
+  id: 0,
+};
